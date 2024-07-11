a38eaf4f299de666cb49195daa1811aab3336041
AlbertApaza <102675510+AlbertApaza@users.noreply.github.com>
2024-07-11 14:41:47 -0500

    Update ClienteContext.php

--- features/bootstrap/ClienteContext.php
+++ features/bootstrap/ClienteContext.php
@@ -1,6 +1,8 @@
 <?php
 
 use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
 use Phake;
 use Exception;
 
@@ -8,12 +10,18 @@ use Exception;
 interface ClienteService
 {
     public function crearCliente($data);
+    public function listarClientes();
+    public function editarCliente($idcliente, $data);
+    public function eliminarCliente($idcliente);
+    public function buscarCliente($termino);
+    public function obtenerClientePorId($idcliente);
 }
 
 class ClienteContext implements Context
 {
     private $clienteService;
     private $exception;
+    private $clientes = [];
 
     public function __construct()
     {
@@ -22,136 +30,145 @@ class ClienteContext implements Context
     }
 
     /**
-     * @Given que el Personal navega a la página de administración de clientes
+     * @Given que tengo una instancia del modelo Cliente
      */
-    public function queElPersonalNavegaALaPaginaDeAdministracionDeClientes()
+    public function queTengoUnaInstanciaDelModeloCliente()
     {
         // No se requiere implementación para este método utilizando Phake
     }
 
     /**
-     * @Given selecciona "Crear nuevo cliente"
+     * @Given que existen los siguientes clientes en la base de datos:
      */
-    public function seleccionaCrearNuevoCliente()
+    public function queExistenLosSiguientesClientesEnLaBaseDeDatos(TableNode $clientesTable)
     {
-        // No se requiere implementación para este método utilizando Phake
-    }
-
-    /**
-     * @Given no ingresa datos enviando el formulario
-     */
-    public function noIngresaDatosEnviandoElFormulario()
-    {
-        // Simular comportamiento con Phake
-        Phake::when($this->clienteService)
-            ->crearCliente(Phake::anyParameters())
-            ->thenThrow(new Exception('Los campos del formulario no pueden estar vacíos'));
-    }
-
-    /**
-     * @Given envía el formulario
-     */
-    public function enviaElFormulario()
-    {
-        // Simular comportamiento con Phake
-        try {
-            $this->clienteService->crearCliente([]);
-        } catch (Exception $e) {
-            $this->exception = $e;
+        foreach ($clientesTable as $clienteData) {
+            $this->clientes[] = [
+                'nombre' => $clienteData['nombre'],
+                'apellido' => $clienteData['apellido'],
+                'correo' => $clienteData['correo'],
+                'iddocumento' => $clienteData['iddocumento'],
+                'documento' => $clienteData['documento'],
+                'telefono' => $clienteData['telefono'],
+            ];
         }
     }
 
     /**
-     * @Then el sistema muestra un mensaje de error "Los campos del formulario no pueden estar vacíos"
+     * @When solicito la lista de clientes
      */
-    public function elSistemaMuestraUnMensajeDeError()
+    public function solicitoLaListaDeClientes()
     {
-        // Verificar que se muestra el mensaje de error utilizando Phake
-        if ($this->exception && $this->exception->getMessage() === 'Los campos del formulario no pueden estar vacíos') {
-            return true;
+        // Simular comportamiento con Phake
+        Phake::when($this->clienteService)->listarClientes()->thenReturn($this->clientes);
+    }
+
+    /**
+     * @Then la lista debe contener al menos un cliente
+     */
+    public function laListaDebeContenerAlMenosUnCliente()
+    {
+        // No se requiere implementación para este método utilizando Phake
+    }
+
+    /**
+     * @When añado un nuevo cliente con nombre :nombre, apellido :apellido, correo :correo, iddocumento :iddocumento, documento :documento, teléfono :telefono
+     */
+    public function anadoUnNuevoClienteConLosSiguientesDatos($nombre, $apellido, $correo, $iddocumento, $documento, $telefono)
+    {
+        // Simular comportamiento con Phake
+        $clienteData = [
+            'nombre' => $nombre,
+            'apellido' => $apellido,
+            'correo' => $correo,
+            'iddocumento' => $iddocumento,
+            'documento' => $documento,
+            'telefono' => $telefono,
+        ];
+        $this->clientes[] = $clienteData;
+        Phake::when($this->clienteService)->crearCliente($clienteData)->thenReturn(true);
+    }
+
+    /**
+     * @Then la lista de clientes debe incluir :nombreCompleto
+     */
+    public function laListaDeClientesDebeIncluir($nombreCompleto)
+    {
+        // No se requiere implementación para este método utilizando Phake
+    }
+
+    /**
+     * @When busco clientes con el término :termino
+     */
+    public function buscoClientesConElTermino($termino)
+    {
+        // Simular comportamiento con Phake
+        $clientesEncontrados = array_filter($this->clientes, function ($cliente) use ($termino) {
+            return strpos($cliente['nombre'] . ' ' . $cliente['apellido'], $termino) !== false;
+        });
+        Phake::when($this->clienteService)->buscarCliente($termino)->thenReturn($clientesEncontrados);
+    }
+
+    /**
+     * @Then los resultados de la búsqueda deben incluir :nombreCompleto
+     */
+    public function losResultadosDeLaBusquedaDebenIncluir($nombreCompleto)
+    {
+        // No se requiere implementación para este método utilizando Phake
+    }
+
+    /**
+     * @When intento añadir un nuevo cliente sin completar todos los campos requeridos
+     */
+    public function intentoAnadirUnNuevoClienteSinCompletarTodosLosCamposRequeridos()
+    {
+        // Simular comportamiento con Phake
+        Phake::when($this->clienteService)->crearCliente(Phake::anyParameters())->thenThrow(new Exception('Los campos del formulario no pueden estar vacíos'));
+    }
+
+    /**
+     * @When intento editar el cliente con idcliente :idcliente, estableciendo nombre a :nombre, apellido a :apellido, correo a :correo, iddocumento a :iddocumento, documento a :documento, teléfono a :telefono
+     */
+    public function intentoEditarElClienteConIdclienteEstableciendoDatos($idcliente, $nombre, $apellido, $correo, $iddocumento, $documento, $telefono)
+    {
+        // Simular comportamiento con Phake
+        $clienteData = [
+            'nombre' => $nombre,
+            'apellido' => $apellido,
+            'correo' => $correo,
+            'iddocumento' => $iddocumento,
+            'documento' => $documento,
+            'telefono' => $telefono,
+        ];
+        foreach ($this->clientes as &$cliente) {
+            if ($cliente['idcliente'] == $idcliente) {
+                $cliente = array_merge($cliente, $clienteData);
+                break;
+            }
         }
-
-        throw new Exception('El mensaje de error esperado no fue mostrado.');
+        Phake::when($this->clienteService)->editarCliente($idcliente, $clienteData)->thenReturn(true);
     }
 
     /**
-     * @Given completa el formulario con la información del cliente
+     * @When intento editar el cliente con idcliente :idcliente, estableciendo nombre a :nombre, apellido a :apellido, correo a :correo, iddocumento a :iddocumento, documento a :documento, teléfono a :telefono con datos inválidos
      */
-    public function completaElFormularioConLaInformacionDelCliente()
+    public function intentoEditarElClienteConIdclienteEstableciendoDatosInvalidos($idcliente, $nombre, $apellido, $correo, $iddocumento, $documento, $telefono)
     {
-        // No se requiere implementación para este método utilizando Phake
-    }
-
-    /**
-     * @Then el sistema guarda el nuevo cliente en la base de datos
-     */
-    public function elSistemaGuardaElNuevoClienteEnLaBaseDeDatos()
-    {
-        // No se requiere implementación para este método utilizando Phake
-    }
-
-    /**
-     * @Then muestra un mensaje de confirmación
-     */
-    public function muestraUnMensajeDeConfirmacion()
-    {
-        // Simular mensaje de confirmación con Phake
-        echo "Cliente creado correctamente";
-    }
-
-    /**
-     * @Then el sistema muestra la lista de clientes disponibles
-     */
-    public function elSistemaMuestraLaListaDeClientesDisponibles()
-    {
-        // No se requiere implementación para este método utilizando Phake
-    }
-
-    /**
-     * @Given selecciona un cliente para actualizar
-     */
-    public function seleccionaUnClienteParaActualizar()
-    {
-        // No se requiere implementación para este método utilizando Phake
-    }
-
-    /**
-     * @Given modifica la información del cliente
-     */
-    public function modificaLaInformacionDelCliente()
-    {
-        // No se requiere implementación para este método utilizando Phake
-    }
-
-    /**
-     * @Then el sistema actualiza el cliente en la base de datos
-     */
-    public function elSistemaActualizaElClienteEnLaBaseDeDatos()
-    {
-        // No se requiere implementación para este método utilizando Phake
-    }
-
-    /**
-     * @Given selecciona un cliente para eliminar
-     */
-    public function seleccionaUnClienteParaEliminar()
-    {
-        // No se requiere implementación para este método utilizando Phake
-    }
-
-    /**
-     * @Given confirma la eliminación
-     */
-    public function confirmaLaEliminacion()
-    {
-        // No se requiere implementación para este método utilizando Phake
-    }
-
-    /**
-     * @Then el sistema elimina el cliente de la base de datos
-     */
-    public function elSistemaEliminaElClienteDeLaBaseDeDatos()
-    {
-        // No se requiere implementación para este método utilizando Phake
+        // Simular comportamiento con Phake
+        $clienteData = [
+            'nombre' => $nombre,
+            'apellido' => $apellido,
+            'correo' => $correo,
+            'iddocumento' => $iddocumento,
+            'documento' => $documento,
+            'telefono' => $telefono,
+        ];
+        foreach ($this->clientes as &$cliente) {
+            if ($cliente['idcliente'] == $idcliente) {
+                $cliente = array_merge($cliente, $clienteData);
+                break;
+            }
+        }
+        Phake::when($this->clienteService)->editarCliente($idcliente, $clienteData)->thenThrow(new Exception('Los campos del formulario no pueden estar vacíos'));
     }
 }
